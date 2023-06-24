f0105cdd310db64c78151f635b5ac3cc762a61b7
Mohamed KADI <handskadi@gmail.com>
2023-06-24 11:19:58 +0000

    Grade System init

---
+++ gradesys.py
@@ -0,0 +1,12 @@
+print("Enter mark: ", end =" ");
+mark = int(input())
+if mark >= 90:
+    print("A")
+elif mark >= 80 and  mark < 90:
+    print("B")
+elif mark >= 70 and mark < 80:
+    print("C")
+elif mark >= 60 and mark < 70:
+    print("D")
+else:
+    print("E")
