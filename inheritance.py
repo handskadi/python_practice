9fa19439f7ada6bfac8f2a17241158a083d3cf09
Mohamed KADI <handskadi@gmail.com>
2023-07-11 09:58:15 +0000

    Inhertance : Parent class

---
+++ inheritance.py
@@ -0,0 +1,14 @@
+""" Normal parent Class"""
+
+class Person(object):
+    """ Constructor """
+    def __init__(self, name, id):
+        self.name = name
+        self.id = id
+    """ To check if this person is an employee """
+    def Display(self):
+        print(self.name, self.id)
+
+"""Driver code"""
+emp = Person("Mohamed", 1988)
+emp.Display()
