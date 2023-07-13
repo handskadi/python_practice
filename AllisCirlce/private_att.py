22a829e3e450108c5b278e2d52a6798e30830587
Mohamed KADI <handskadi@gmail.com>
2023-07-13 16:58:08 +0000

    Bank Program pythn

--- AllisCirlce/private_att.py
+++ AllisCirlce/private_att.py
@@ -1,5 +1,6 @@
 class BankAccount:
-    def __init__(self, account_number, balance, pin):
+    def __init__(self, account_holder, account_number, balance, pin):
+        self.__account_holder = account_holder
         self.__account_number = account_number
         self.__balance = balance
         self.__pin = pin
@@ -8,21 +9,23 @@ class BankAccount:
         print(f"Account balance: {self.__balance}")
     def display_account(self):
         print(f"Account Number: {self.__account_number}")
-
-my_account = BankAccount("456382945627384950112746", 20000, 2023)
+    def display_account_holder(self):
+        print(f"Account holder: {self.__account_holder}")
+my_account = BankAccount("Mohamed KADI", "456382945627384950112746", 20000, 2023)
 
 input_pin = int(input("Enter your PIN: "))
 
 if input_pin == my_account._BankAccount__pin:
     print()
-    my_account.display_balance()
+    my_account.display_account_holder()
     my_account.display_account()
+    my_account.display_balance()
     print()
 
     def check_input():
         input_choise = int(input("Choose Operation:\n\t1- Check my Balance\n\t2-  Withdraw \n\t3- Exit \n"))
         if input_choise == 1:
-            print(f"Your current balance: {my_account._BankAccount__balance}")
+            print(f"Mr {my_account._BankAccount__account_holder}: Your current balance: {my_account._BankAccount__balance}")
             check_input()
         if input_choise == 2:
             amount = int(input("Enter your amount: "))
@@ -31,7 +34,7 @@ if input_pin == my_account._BankAccount__pin:
             check_input()
         elif input_choise == 3:
             print("Good bye! have a nice day!")
-            pass
+            exit(0)
         else:
             print("Invalid: back to main Menu")
             check_input()
